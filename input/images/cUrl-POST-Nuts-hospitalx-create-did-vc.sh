ad26798d43284fb3e375aa7c8718f7e88f780480
Roland Groen <roland@headease.nl>
2024-09-10 22:43:54 +0200

    Add the access_token request for the CPS>Hospital calls and update Docker configs for CPS/hospital NUTS nodes
    Refactor scripts to distinguish between hospital and CPS VC documents. Update docker-compose configs to add homemonitoring and careplanservice policies for respective NUTS nodes.

--- input/images/cUrl-POST-Nuts-hospitalx-create-did-vc.sh
+++ input/images/cUrl-POST-Nuts-hospitalx-create-did-vc.sh
@@ -39,7 +39,7 @@ CPS_DID=$(echo "$CPS_DID_DOC" | jq -r .documents[0].id)
 echo "$CPS_DID"
 
 # This will create a new VC on the CPS NUTS node, issued to the hospital.
-VC_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vcr/v2/issuer/vc" \
+HOSPITAL_VC_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vcr/v2/issuer/vc" \
 --header 'Content-Type: application/json' \
 --header 'Accept: application/json' \
 --data-raw "{
@@ -60,16 +60,51 @@ VC_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vcr/v2/issuer/vc" \
     \"withStatusList2021Revocation\": false
   }")
 
-echo "$VC_DOC"
+echo "$HOSPITAL_VC_DOC"
 
 # This will create a post the VC on the hospital NUTS node.
 curl --location "$HOSPITAL_INTERNAL_API/internal/vcr/v2/holder/$HOSPITAL_DID/vc" \
   --header 'Content-Type: application/json' \
-  --data "$VC_DOC"
+  --data "$HOSPITAL_VC_DOC"
 
 # Superfluous call to list the VSs.
 curl --location "$HOSPITAL_INTERNAL_API/internal/vcr/v2/holder/$HOSPITAL_DID/vc" \
 --header 'Accept: application/json'
+
+# This will create a new VC on the CPS NUTS node, issued to the hospital.
+CPS_VC_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vcr/v2/issuer/vc" \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--data-raw "{
+    \"@context\": [
+      \"https://www.w3.org/2018/credentials/v1\",
+      \"https://nuts.nl/credentials/2024\"
+    ],
+    \"type\": \"NutsUraCredential\",
+    \"issuer\": \"${CPS_DID}\",
+    \"credentialSubject\": {
+      \"id\": \"${CPS_DID}\",
+      \"organization\": {
+        \"ura\": \"${URA}\",
+        \"name\": \"${CPS_SUBJECT}\",
+        \"city\": \"${CITY}\"
+      }
+    },
+    \"withStatusList2021Revocation\": false
+  }")
+
+echo "$CPS_VC_DOC"
+
+# This will create a post the VC on the CPS NUTS node.
+curl --location "$CPS_INTERNAL_API/internal/vcr/v2/holder/$CPS_DID/vc" \
+  --header 'Content-Type: application/json' \
+  --data "$CPS_VC_DOC"
+
+# Superfluous call to list the VSs.
+curl --location "$CPS_INTERNAL_API/internal/vcr/v2/holder/$CPS_DID/vc" \
+--header 'Accept: application/json'
+
+## -- UC: access the CPS FHIR API from the hospital to CPS --
 
 # The next item needs the public host name of the CPS NUTS node, this is done by extracting it from the DID.
 # In a more realistic situation this would be either known or available in a DID document.
@@ -80,7 +115,7 @@ echo "$CPS_HOST"
 # This call gets a access token on the hospital's NUTS node to access data on the CPS FHIR environment.
 ACCESS_TOKEN_JSON=$(curl --location "$HOSPITAL_INTERNAL_API/internal/auth/v2/$HOSPITAL_SUBJECT/request-service-access-token" \
   --header 'Content-Type: application/json' \
---data-raw "{
+  --data-raw "{
     \"authorization_server\": \"https://$CPS_HOST/oauth2/$CPS_SUBJECT\",
     \"scope\": \"careplanservice\"
   }")
@@ -94,3 +129,21 @@ echo "$ACCESS_TOKEN"
 curl --location "$CPS_INTERNAL_API/internal/auth/v2/accesstoken/introspect_extended" \
   --header "Content-Type: application/x-www-form-urlencoded" \
   --data "token=$ACCESS_TOKEN"
+
+## -- UC: access the hospital FHIR API from CPS --
+
+# The next item needs the public host name of the CPS NUTS node, this is done by extracting it from the DID.
+# In a more realistic situation this would be either known or available in a DID document.
+HOSPITAL_DID_PARTS=(${HOSPITAL_DID//:/ })
+HOSPITAL_HOST="${HOSPITAL_DID_PARTS[2]}"
+echo "$HOSPITAL_HOST"
+
+# This call gets a access token on the hospital's NUTS node to access data on the CPS FHIR environment.
+ACCESS_TOKEN_JSON=$(curl --location "$CPS_INTERNAL_API/internal/auth/v2/$CPS_SUBJECT/request-service-access-token" \
+  --header 'Content-Type: application/json' \
+  --data-raw "{
+    \"authorization_server\": \"https://$HOSPITAL_HOST/oauth2/$HOSPITAL_SUBJECT\",
+    \"scope\": \"homemonitoring\"
+  }")
+# Get the access_token from the JSON
+echo "$ACCESS_TOKEN_JSON"
