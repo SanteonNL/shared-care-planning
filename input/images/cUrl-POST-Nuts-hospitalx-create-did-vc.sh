549e2aa806c3e090717e58e94b612c4a03643111
Roland Groen <roland@headease.nl>
2024-09-10 13:01:31 +0200

    Refactor API configuration and add token handling
    Updated API configurations and fixed variable names in cURL script. Added handling for access tokens, including obtaining and verifying tokens for improved API interactions. Reflected these changes in the corresponding Docker Compose setup file.

--- input/images/cUrl-POST-Nuts-hospitalx-create-did-vc.sh
+++ input/images/cUrl-POST-Nuts-hospitalx-create-did-vc.sh
@@ -1,33 +1,44 @@
-export NAME="hospital_x"
-export URA="124453423"
-export CITY="Amsterdam"
+# This file assumes a running docker-compose setup as defined in docker-compose-nuts-nodes.sh
+# Please note that
+
+# The docker compose and ngrok need to run to make this work.
+# The internal URL of the hospital NUTS node
 export HOSPITAL_INTERNAL_API="http://localhost:8081"
+# The internal URL of the care plan service (CPS) NUTS node
 export CPS_INTERNAL_API="http://localhost:8181"
 
+# The values are used as subject in the DID creation of both the hospital and the care plan service.
+export HOSPITAL_SUBJECT="hospital_x"
+export CPS_SUBJECT="care_plan_service"
+# The values fro the hospital.
+export URA="124453423"
+export CITY="Amsterdam"
 
-
-echo "Create $NAME DID"
+# This will create a new DID on the hospital side.
+echo "Create $HOSPITAL_SUBJECT DID"
 HOSPITAL_DID_DOC=$(curl --location "${HOSPITAL_INTERNAL_API}/internal/vdr/v2/subject" \
 --header 'Content-Type: application/json' \
 --header 'Accept: application/json' \
 --data "{
-  \"subject\": \"${NAME}\"
+  \"subject\": \"${HOSPITAL_SUBJECT}\"
 }")
 echo "$HOSPITAL_DID_DOC"
 HOSPITAL_DID=$(echo "$HOSPITAL_DID_DOC" | jq -r .documents[0].id)
 echo "$HOSPITAL_DID"
 
-echo "Create CPS DID"
+# This will create a new DID on the CPS side.
+echo "Create $CPS_SUBJECT DID"
 CPS_DID_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vdr/v2/subject" \
 --header 'Content-Type: application/json' \
 --header 'Accept: application/json' \
 --data "{
-  \"subject\": \"CPS\"
+  \"subject\": \"$CPS_SUBJECT\"
 }")
 echo "$CPS_DID_DOC"
 CPS_DID=$(echo "$CPS_DID_DOC" | jq -r .documents[0].id)
 echo "$CPS_DID"
 
+# This will create a new VC on the CPS NUTS node, issued to the hospital.
 VC_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vcr/v2/issuer/vc" \
 --header 'Content-Type: application/json' \
 --header 'Accept: application/json' \
@@ -42,7 +53,7 @@ VC_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vcr/v2/issuer/vc" \
       \"id\": \"${HOSPITAL_DID}\",
       \"organization\": {
         \"ura\": \"${URA}\",
-        \"name\": \"${NAME}\",
+        \"name\": \"${HOSPITAL_SUBJECT}\",
         \"city\": \"${CITY}\"
       }
     },
@@ -51,17 +62,35 @@ VC_DOC=$(curl --location "${CPS_INTERNAL_API}/internal/vcr/v2/issuer/vc" \
 
 echo "$VC_DOC"
 
-
+# This will create a post the VC on the hospital NUTS node.
 curl --location "$HOSPITAL_INTERNAL_API/internal/vcr/v2/holder/$HOSPITAL_DID/vc" \
   --header 'Content-Type: application/json' \
   --data "$VC_DOC"
 
-curl --location "$HOSPITAL_INTERNAL_API/internal/vcr/v2/holder/did:web:d996-195-240-138-159.ngrok-free.app:iam:221ab978-ecea-48f8-b146-3450d8e6f386/vc" \
+# Superfluous call to list the VSs.
+curl --location "$HOSPITAL_INTERNAL_API/internal/vcr/v2/holder/$HOSPITAL_DID/vc" \
 --header 'Accept: application/json'
 
-curl --location "$HOSPITAL_INTERNAL_API/internal/auth/v2/$HOSPITAL_DID/request-service-access-token" \
+# The next item needs the public host name of the CPS NUTS node, this is done by extracting it from the DID.
+# In a more realistic situation this would be either known or available in a DID document.
+CPS_DID_PARTS=("${CPS_DID//:/ }")
+CPS_HOST="${CPS_DID_PARTS[2]}"
+
+# This call gets a access token on the hospital's NUTS node to access data on the CPS FHIR environment.
+ACCESS_TOKEN_JSON=$(curl --location "$HOSPITAL_INTERNAL_API/internal/auth/v2/$HOSPITAL_SUBJECT/request-service-access-token" \
   --header 'Content-Type: application/json' \
 --data-raw "{
-    \"verifier\": \"${CPS_DID}\",
-    \"scope\": \"scp_cpc\"
-  }"
+    \"authorization_server\": \"https://$CPS_HOST/oauth2/$CPS_SUBJECT\",
+    \"scope\": \"careplanservice\",
+    \"credentials\": [ $VC_DOC ]
+  }")
+
+# Get the access_token from the JSON
+echo "$ACCESS_TOKEN_JSON"
+ACCESS_TOKEN=$(echo "$ACCESS_TOKEN_JSON" | jq -r .access_token)
+echo "$ACCESS_TOKEN"
+
+# The verification step: the CPS NUTS node will validate the token.
+curl --location "$CPS_INTERNAL_API/internal/auth/v2/accesstoken/introspect_extended" \
+  --header "Content-Type: application/x-www-form-urlencoded" \
+  --data "token=$ACCESS_TOKEN"
--- input/images/docker-compose-nuts-nodes.sh
+++ input/images/docker-compose-nuts-nodes.sh
@@ -1,7 +1,7 @@
 # Please run the following command and replace the HOSPITAL_EXTERNAL_API with the value of the external URL created by ngrok
 # ngrok http 8080
 # Please run the following command and replace the CPS_EXTERNAL_API with the value of the external URL created by ngrok
-# ngrok http 8980
+# ngrok http 8180
 
 export HOSPITAL_EXTERNAL_API="https://....ngrok-free.app"
 export CPS_EXTERNAL_API="https://....ngrok-free.app"
